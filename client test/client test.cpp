65f0b910410c0f9fbb4f3a1a293a96ccd546fde7
Uttam Arora <Uarora3486@conestogac.on.ca>
2025-02-23 01:13:12 -0500

    server connection setup is added

--- client test/client test.cpp
+++ client test/client test.cpp
@@ -11,10 +11,22 @@ using namespace std;
 
 void sendLogMessage(const string& serverIP, int port, const string& message) {
     WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    WSAStartup(MAKEWORD(2, 2), &wsaData); //initializing
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET) {
-        cerr << "Socket creation failed." << endl;
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0); //creating a socket here. it will communicate
+    if (sock == INVALID_SOCKET) { //checking
+        cerr << "Socket creation failed." << endl; //error message if it fails
+        return;
+    }
+
+    sockaddr_in serverAddr; //server address will be held
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr);
+
+    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) { //connection to server with IP and port
+        cerr << "Connection to server failed." << endl; //error message if it fails
+        closesocket(sock); //closes after sending the message
+        WSACleanup();
         return;
     }
