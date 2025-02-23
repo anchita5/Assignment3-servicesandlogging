768330a733581d623443e190cc44596c64039442
Uttam Arora <Uarora3486@conestogac.on.ca>
2025-02-22 23:09:12 -0500

    sendLogMessage function added to send log messages

--- client test/client test.cpp
+++ client test/client test.cpp
@@ -6,3 +6,15 @@
 #pragma comment(lib, "Ws2_32.lib")
 //comment to check
 //uttam commented
+
+using namespace std;
+
+void sendLogMessage(const string& serverIP, int port, const string& message) {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        cerr << "Socket creation failed." << endl;
+        return;
+    }
