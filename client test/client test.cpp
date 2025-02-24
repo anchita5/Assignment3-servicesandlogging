42443a9b4d2c1dd7477eb4126db1e93bcb07cfa4
Uttam Arora <Uarora3486@conestogac.on.ca>
2025-02-23 19:46:24 -0500

    all done

--- client test/client test.cpp
+++ client test/client test.cpp
@@ -79,7 +79,7 @@ int main(int argc, char* argv[]) {
         }
     }
     else {
-        cout << "Invalid choice. Exiting." << endl;
+        cout << "Invalid choice. Exiting." << endl; //message
     }
 
     return 0;
